c2d83897cd1d70d9028a99f4280e8e9b4da5b175
mugbya <mugbya@gmail.com>
2017-03-02 21:41:20 +0800

    write cookie

--- user/view.py
+++ user/view.py
@@ -12,20 +12,18 @@ logger = logging.getLogger(__name__)
 
 @bp.route('/login/', methods=['GET', 'POST'])
 async def login(request):
-    logger.debug('debug message')
-    logger.info('info message')
+    if request.method == 'POST':
+        username = request.form.get('username', '')
+        password = request.form.get('password', '')
 
-    logger.warn('warn message')
-    logger.error('error message')
-    logger.critical('critical message')
-    # if request.method == 'POST':
-    #     username = request.form.get('username', '')
-    #     password = request.form.get('password', '')
-    #
-    #     user = await User.filter(username=username, password=password)
-    #     if user:
-    #         print('sdfsd')
-    #     return json({})
-    # else:
-    #     pass
-
+        user_list = await User.filter(nickname=username, password=password)
+        if user_list:
+            response = json(user_list)
+            response.cookies[username] = username
+            response.cookies[username]['domain'] = '.gotta-go-fast.com'
+            response.cookies[username]['httponly'] = True
+            return response
+        else:
+            return json({'msg': 'user not exist'})
+    else:
+        pass
