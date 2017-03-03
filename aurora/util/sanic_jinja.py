705efd7bcdaf764483bfcb02c6a5b8eb1d360cb3
mugbya <mugbya@gmail.com>
2017-03-03 22:40:28 +0800

    sanic_jinja 使用类方法调用

--- aurora/util/sanic_jinja.py
+++ aurora/util/sanic_jinja.py
@@ -2,36 +2,36 @@ from sanic.response import html
 from jinja2 import Environment, PackageLoader
 from config.settings import PACKAGE_NAME, TEMPLATES
 
+
 class SanicJinja2:
-    def __init__(self, loader=None, **kwargs):
-        self.env = Environment(**kwargs)
-        if not loader:
-            loader = PackageLoader(PACKAGE_NAME, TEMPLATES)
+    env = Environment()
+    loader = PackageLoader(PACKAGE_NAME, TEMPLATES)
+    env.loader = loader
 
-        self.env.loader = loader
-
-    def fake_trans(self, text, *args, **kwargs):
-        return text
-
-    def update_request_context(self, request, context):
+    @classmethod
+    def update_request_context(cls, request, context):
         context.setdefault('request', request)
 
-    async def render_string_async(self, template, request, **context):
-        self.update_request_context(request, context)
-        return await self.env.get_template(template).render_async(**context)
+    @classmethod
+    async def render_string_async(cls, template, request, **context):
+        cls.update_request_context(request, context)
+        return await cls.env.get_template(template).render_async(**context)
 
-    async def render_async(self, template, request, **context):
-        return html(await self.render_string_async(template, request,
+    @classmethod
+    async def render_async(cls, template, request, **context):
+        return html(await cls.render_string_async(template, request,
                                                    **context))
 
-    def render_string(self, template, request, **context):
-        self.update_request_context(request, context)
-        return self.env.get_template(template).render(**context)
+    @classmethod
+    def render_string(cls, template, request, **context):
+        cls.update_request_context(request, context)
+        return cls.env.get_template(template).render(**context)
 
-    def render(self, template, request, **context):
-        return html(self.render_string(template, request, **context))
+    @classmethod
+    def render(cls, template, request, **context):
+        return html(cls.render_string(template, request, **context))
 
 
 def render(template, request, **context):
-    return html(SanicJinja2().render_string(template, request, **context))
+    return html(SanicJinja2.render_string(template, request, **context))
 
